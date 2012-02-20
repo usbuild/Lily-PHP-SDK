f4ad39ebb3614efc909caa85d16cd1bfe6541213
张启超 <njuzhangqichao@gmail.com>
2012-02-20 18:30:21 +0800

    基本完成

--- file.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-	require_once("LilyClient.php");
-	$client = new LilyClient;
-	$cookie = $client->getCookie('usbuild', '19911022bbs');
-	echo $cookie;
-	$file = fopen("cookie.txt", 'w+');
-	fwrite($file, $cookie);
-	fclose($file);
-?>
